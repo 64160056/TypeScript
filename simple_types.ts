18ee2061347a669f116259a40bdbc835333c1e01
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-11-27 13:02:56 +0700

    Simple Type

---
+++ simple_types.ts
@@ -0,0 +1,5 @@
+let firstName: string = "Dylan";
+//firstName = 33:
+
+console.log(firstName);
+console.log(typeof firstName);
---
+++ simple_types2.ts
@@ -0,0 +1,4 @@
+let json:any = JSON.parse("55"); //.parse = การแปลง
+console.log(typeof json);
+json = "55";
+console.log(typeof json);
