8f4c23c2c2b449b48dbf45bae34ae934f7e8d2f1
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-11-27 23:29:21 +0700

    Interface1 agian

--- interface1.ts
+++ interface1.ts
@@ -3,9 +3,19 @@ interface Rectangle {
     heigth : number;
 
 }
+interface ColoredRectangle extends Rectangle{
+    color: string
+}
 
 const rectangie: Rectangle = {
     width : 20,
     heigth : 10,
 }
 console.log(rectangie)
+
+const coloredRectangle: ColoredRectangle = {
+    width : 20,
+    heigth : 10,
+    color: "red"
+}
+console.log(coloredRectangle)
